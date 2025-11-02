e1c4fbede570fc5eb096dfb622c101270050d0a9
Adam Schubert <schubertadam@outlook.com>
2025-11-02 08:45:20 +0100

    feat(ui): Create 'auth' layout for shared authentication views

---
+++ app/View/Components/Layouts/Auth.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\View\Components\Layouts;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Auth extends Component
+{
+    public string $title = "";
+
+    public function __construct(string $title = "")
+    {
+        if (!empty($title)) {
+            $this->title = $title . " | ";
+        }
+
+        $this->title .= config('app.name');
+    }
+
+    public function render(): View
+    {
+        return view('components.layouts.auth');
+    }
+}
