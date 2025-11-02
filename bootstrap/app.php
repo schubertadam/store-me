8a60c07201ff317d2efeca164174196183559ee6
Adam Schubert <schubertadam@outlook.com>
2025-11-02 22:52:37 +0100

    chore(config): Refine global exception handling and middleware redirects

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Exceptions\LockedOutException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Http\Request as ExceptionRequest;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,8 +26,30 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectGuestsTo(fn (Request $request) => route('login.index'));
+        $middleware->redirectUsersTo(fn (Request $request) => route('dashboard'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (Throwable $e, ExceptionRequest $request) {
+            if ($e instanceof ValidationException) {
+                return back()->withErrors($e->errors())->withInput();
+            }
+
+            if ($e instanceof AuthenticationException) {
+                return redirect()->route('login.index')
+                    ->withErrors(['custom' => __('Your session has expired. Please log in again.')])->withInput();
+            }
+
+            if ($e instanceof Exception) {
+                if ($e instanceof LockedOutException) {
+                    event(new Lockout($request));
+                }
+
+                if (!config('app.debug')) {
+                    return back()->withErrors(['custom' => __('Something went wrong, please contact us!')])->withInput();
+                }
+            }
+
+            return null;
+        });
     })->create();
