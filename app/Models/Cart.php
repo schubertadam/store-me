6f267c24d8b06d53ea10f1f85667ea71148638ae
Adam Schubert <schubertadam@outlook.com>
2025-11-27 22:29:10 +0100

    feat(ecommerce): Implement database structure and models for shopping cart

---
+++ app/Models/Cart.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Cart extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'session_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_11_27_203403_create_cart_items_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Cart::class)->constrained('carts');
+            $table->foreignIdFor(Product::class)->constrained('products');
+            $table->smallInteger('quantity');
+            $table->decimal('price_at_addition');
+            $table->timestamps();
+
+            $table->unique(['cart_id', 'product_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
