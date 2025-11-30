ef65b3d2b6742c59e95892675cacf919f69c1cca
Adam Schubert <schubertadam@outlook.com>
2025-11-30 15:20:11 +0100

    feat(cart): Define hasMany relationship to CartItem model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
@@ -15,5 +16,10 @@ class Cart extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function items(): HasMany|Cart
+    {
+        return $this->hasMany(CartItem::class);
     }
 }
