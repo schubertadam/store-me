e3400fc66eeed88b4c270366c9a88c0af2c79e2e
Adam Schubert <schubertadam@outlook.com>
2025-12-07 09:28:18 +0100

    feat(db): create demo data inside DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,10 +18,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::query()->create([
+        $customer = Role::query()->create(['name' => 'customer']);
+        $adminstrator = Role::query()->create(['name' => 'adminstrator']);
+
+        Permission::query()->insert([
+            ['group' => 'user', 'name' => 'create-user'],
+            ['group' => 'user', 'name' => 'view-user'],
+            ['group' => 'user', 'name' => 'edit-user'],
+            ['group' => 'user', 'name' => 'delete-user'],
+
+            ['group' => 'category', 'name' => 'create-category'],
+            ['group' => 'category', 'name' => 'view-category'],
+            ['group' => 'category', 'name' => 'edit-category'],
+            ['group' => 'category', 'name' => 'delete-category'],
+
+            ['group' => 'product', 'name' => 'create-product'],
+            ['group' => 'product', 'name' => 'view-product'],
+            ['group' => 'product', 'name' => 'edit-product'],
+            ['group' => 'product', 'name' => 'delete-product'],
+        ]);
+
+        foreach (Permission::all() as $permission) {
+            $adminstrator->permissions()->attach($permission->id);
+        }
+
+        $admin = User::query()->create([
             'name' => 'Administrator',
             'email' => 'admin@admin.com',
             'password' => 'admin',
         ]);
+
+        $admin->roles()->attach($adminstrator->id);
     }
 }
